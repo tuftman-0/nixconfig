2db93349e228327e015942891a81c1c1c915babc
tuftman-0 <joshmartin1010@gmail.com>
2024-12-06 11:56:05 -0500

    added old version of bibata cursors >:(

--- flake.nix
+++ flake.nix
@@ -3,15 +3,17 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    stable.url = "github:nixos/nixpkgs?ref=nixos-23.11";
     ki-editor.url = "github:ki-editor/ki-editor";
   };
 
-  outputs = { self, nixpkgs, ki-editor, ...  }: {
+  outputs = { self, nixpkgs, stable, ki-editor, ...  }: {
     nixosConfigurations."nixos" = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
         modules = [
           ({ pkgs, ... } : {system.configurationRevision = nixpkgs.lib.mkIf (self ? rev) self.rev;})
           { environment.systemPackages = [ ki-editor.packages.x86_64-linux.default ]; }
+          { environment.systemPackages = [ stable.legacyPackages.x86_64-linux.bibata-cursors ]; }
           ./configuration.nix
       ];
     };
