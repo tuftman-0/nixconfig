1dc8bc9356a1c3020854b6b5194bbd87c3597d1f
tuftman-0 <28765011+tuftman-0@users.noreply.github.com>
2025-03-10 11:12:50 -0400

    changed hostname to thunkpad

--- flake.nix
+++ flake.nix
@@ -7,17 +7,18 @@
     ki-editor.url = "github:ki-editor/ki-editor";
   };
 
-  outputs = { self, nixpkgs, stable, ki-editor, ...  }: {
-    nixosConfigurations."nixos" = nixpkgs.lib.nixosSystem {
+  outputs = inputs@{ self, nixpkgs, stable, ki-editor, ...  }: {
+    nixosConfigurations."thunkpad" = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
-        modules = [
-          ({ pkgs, ... } : {system.configurationRevision = nixpkgs.lib.mkIf (self ? rev) self.rev;})
-          { environment.systemPackages = [
-            stable.legacyPackages.x86_64-linux.bibata-cursors
-            stable.legacyPackages.x86_64-linux.jetbrains.idea-community
-            ki-editor.packages.x86_64-linux.default
-          ]; }
-          ./configuration.nix
+      specialArgs = { inherit inputs; };
+      modules = [
+        ({ pkgs, ... } : {system.configurationRevision = nixpkgs.lib.mkIf (self ? rev) self.rev;})
+        { environment.systemPackages = [
+          stable.legacyPackages.x86_64-linux.bibata-cursors
+          stable.legacyPackages.x86_64-linux.jetbrains.idea-community
+          ki-editor.packages.x86_64-linux.default
+        ]; }
+        ./configuration.nix
       ];
     };
   };
