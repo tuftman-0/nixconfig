4cd421801604f194322edb2096d9fb4f132a1265
tuftman-0 <28765011+tuftman-0@users.noreply.github.com>
2025-03-21 17:27:08 -0400

    disabled fprintd

--- configuration.nix
+++ configuration.nix
@@ -234,12 +234,12 @@ in
     };
   };
 
-  services.fprintd = {
-    enable = true;
-    tod.enable = true;
-    # tod.driver = pkgs.libfprint-2-tod1-goodix;
-    tod.driver = pkgs.libfprint-2-tod1-vfs0090;
-  };
+  # services.fprintd = {
+  #   enable = true;
+  #   tod.enable = true;
+  #   # tod.driver = pkgs.libfprint-2-tod1-goodix;
+  #   tod.driver = pkgs.libfprint-2-tod1-vfs0090;
+  # };
 
   programs.hyprland = {
     enable = true;
